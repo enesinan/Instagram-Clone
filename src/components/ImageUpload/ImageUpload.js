f179ab7898dee246d0e40a30820ce153820ca327
enes-dev <inane064@gmail.com>
2021-05-21 11:34:47 +0300

    ImageUpload.js component package added for the image uploading

---
+++ src/components/ImageUpload/ImageUpload.js
@@ -0,0 +1,62 @@
+import { Button } from '@material-ui/core';
+import React, {useState} from 'react';
+import { storage, db } from "../../Firebase/firebase"; 
+import firebase from "firebase";
+import './ImageUpload.css';
+
+function ImageUpload({username}) {
+    const [caption, setCaption] = useState('');
+    const [progress, setProgress] = useState(0);
+    const [image, setImage] = useState(null);
+
+    const handleChange = (e) => {
+        if(e.target.files[0]){
+            setImage(e.target.files[0]);
+        }
+    }
+    const handleUpload = () => {
+        const uploadTask = storage.ref(`images/${image.name}`).put(image);
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+                setProgress(progress);
+            },
+            (error) => {
+                console.log(error);
+                alert(error.message);
+            },
+            () => {
+                storage
+                    .ref("images")
+                    .child(image.name)
+                    .getDownloadURL()
+                    .then(url => {
+                        db.collection("posts").add({
+                            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                            caption: caption,
+                            imageUrl: url,
+                            username: username
+                        });
+                        setProgress(0);
+                        setCaption("");
+                        setImage(null);
+                    })
+            }
+        )
+    }
+    return (
+        <div className="imageupload">
+            <progress className="imageupload__progress" value={progress} max="100" />
+            <input type="text" placeholder='Enter a caption...' onChange={event => setCaption(event.target.value)} value={caption} />
+            <input type="file" onChange={handleChange} />
+            <Button onClick={handleUpload}>
+                Upload
+            </Button>
+        </div>
+    )
+}
+
+export default ImageUpload
